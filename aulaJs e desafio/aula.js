2be83751f32753946ee48f534f5acca8c8c05b96
Guganand <fernando-guga@hotmail.com>
2022-10-30 20:26:31 -0300

    primeiro desafio

---
+++ aulaJs e desafio/aula.js
@@ -0,0 +1,9 @@
+let variavel = 10; //variavel pode mudar seu valor
+
+variavel = 20;
+
+
+//const pi = 3.14; //const=> depois de atribuir o valor,o mesmo não pode ser alterado
+
+
+console.log(variavel);
---
+++ aulaJs e desafio/primeiro_desafio.js
@@ -0,0 +1,16 @@
+/* Faça um programa para caular o valor de uma viagem.
+
+voçe terá 3 variaveis. Sendo elas:
+1- preço do combustivel;
+2- Gasto médio de combustivel do carro por KM;
+3-Distância em Km da viagem;
+
+*/
+
+let precoCombustivel = 5.79;
+let gastoPorKM = 12;
+let distEmKM =1580;
+
+let valorViagem = distEmKM / gastoPorKM * precoCombustivel;
+
+console.log(valorViagem.toFixed(2));
