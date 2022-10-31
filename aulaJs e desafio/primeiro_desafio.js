0820871ba20eb7712e4ad3412fae65112f0d0664
Guganand <fernando-guga@hotmail.com>
2022-10-30 22:55:02 -0300

    adicionando aulaJs_02

--- aulaJs e desafio/primeiro_desafio.js
+++ aulaJs e desafio/primeiro_desafio.js
@@ -1,4 +1,4 @@
-/* Faça um programa para caular o valor de uma viagem.
+/* Faça um programa para calcular o valor de uma viagem.
 
 voçe terá 3 variaveis. Sendo elas:
 1- preço do combustivel;
@@ -13,4 +13,4 @@ let distEmKM =1580;
 
 let valorViagem = distEmKM / gastoPorKM * precoCombustivel;
 
-console.log(valorViagem.toFixed(2));
+console.log(valorViagem.toFixed(2)); // toFixed(2)=> arredonda um valor quebrado 
